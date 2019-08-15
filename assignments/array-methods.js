52b47cbddb4cb20f83d566f9852a0558597ad709
warndawg <kevin.warner520@gmail.com>
2019-08-14 21:05:53 -0600

    finished project

--- assignments/array-methods.js
+++ assignments/array-methods.js
@@ -56,28 +56,53 @@ const runners = [{"id":1,"first_name":"Charmain","last_name":"Seiler","email":"c
 // ==== Challenge 1: Use .forEach() ====
 // The event director needs both the first and last names of each runner for their running bibs.  Combine both the first and last names into a new array called fullName. 
 let fullName = [];
+runners.forEach(r => fullName.push(r.first_name + ' ' + r.last_name));
 console.log(fullName);
 
 // ==== Challenge 2: Use .map() ====
 // The event director needs to have all the runner's first names converted to uppercase because the director BECAME DRUNK WITH POWER. Convert each first name into all caps and log the result
 let allCaps = [];
+runners.map( r =>allCaps.push(r.first_name.toUpperCase()));
 console.log(allCaps); 
 
 // ==== Challenge 3: Use .filter() ====
 // The large shirts won't be available for the event due to an ordering issue.  Get a list of runners with large sized shirts so they can choose a different size. Return an array named largeShirts that contains information about the runners that have a shirt size of L and log the result
 let largeShirts = [];
+largeShirts.push(
+    runners.filter((size)=>{
+        return size.shirt_size === 'L';    
+    })
+);
 console.log(largeShirts);
 
 // ==== Challenge 4: Use .reduce() ====
 // The donations need to be tallied up and reported for tax purposes. Add up all the donations into a ticketPriceTotal array and log the result
 let ticketPriceTotal = [];
+ticketPriceTotal.push(
+    runners.reduce((total, money)=>{
+        return total += money.donation; 
+    }, 0)
+);
 console.log(ticketPriceTotal);
 
 // ==== Challenge 5: Be Creative ====
 // Now that you have used .forEach(), .map(), .filter(), and .reduce().  I want you to think of potential problems you could solve given the data set and the 5k fun run theme.  Try to create and then solve 3 unique problems using one or many of the array methods listed above.
 
-// Problem 1
+// Problem 1 email list to send out newsletters 
+let emailList = [];
+runners.forEach(r => emailList.push(r.email));
+console.log(emailList);
 
-// Problem 2
+// Problem 2 shout out to all the companies represented
+let shoutOutComs = [];
+runners.forEach(r => shoutOutComs.push(r.company_name));
+console.log("Lets give a shout out to the compenies represented " + shoutOutComs);
 
-// Problem 3
+// Problem 3 free photo to all runners that donnated 100 dolars or more
+let photo = [];
+photo.push(
+    runners.filter((d)=>{
+        return d.donation >= 100;    
+    })
+);
+console.log(photo);
--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,10 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+const Hello = 'World';
+function returnHello () {
+  return Hello;
+}
+returnHello();
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
